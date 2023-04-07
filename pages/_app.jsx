f1510bc1520b189b7880ed3a679cf422ec337f66
Replit user <>
2023-04-07 04:33:56 +0000

    removed amplitude and vercel packages

--- pages/_app.jsx
+++ pages/_app.jsx
@@ -1,12 +1,10 @@
 import 'nextra-theme-blog/style.css'
 import 'tailwindcss/tailwind.css'
-import { Analytics } from '@vercel/analytics/react';
 
 export default function Nextra({ Component, pageProps }) {
   return (
     <>
       <Component {...pageProps} />
-      <Analytics />
     </>
   );
 }
